cb18345327958541736666e84932004f3d60c4fb
ryan <ryan.williams93@aol.com>
2022-01-12 18:37:07 -0600

    Write Failing Tests for getStats and getInventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,6 @@
-const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 
-test ('creates a player object', () => {
+test('creates a player object', () => {
     const player = new Player('Ryan');
 
     expect(player.name).toBe('Ryan');
@@ -11,5 +10,23 @@ test ('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
 
-})
+test("gets player's stats as an object", () => {
+    const player = new Player('Ryan');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');    
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Ryan');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getnventory()).toEqual(false);
+});
